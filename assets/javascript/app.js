15bca1cdde1b27d7b5b99125f840b764b0197915
Geoff's Macbook <geoffsmacbook@Geoffss-MacBook-Pro.local>
2018-04-01 13:31:09 -0400

    updated recognition logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,13 +14,39 @@ $(document).ready(function(){
 
   let sentenceArray = [];
   let ofEqualsArray = ['of', 'equals', 'is'];
+  let elementMatchCount = 0;
   let idText;
   let classText;
   let submittedChat;
+  let elementObjectIndex;
 
   let final;
   let interim;
   let recognizing;
+
+  const elementsObjectsArray = [
+    {
+      position: 0,
+      name: 'div',
+      openTag: '<div>',
+      closingTag: '</div>',
+      aliases: ['division', 'divider', 'div']
+    },
+    {
+      position: 1,
+      name: 'p',
+      openTag: '<p>',
+      closingTag: '</p>',
+      aliases: ['paragraph', 'par', 'p']
+    },
+    {
+      position: 2,
+      name: 'h1',
+      openTag: '<h1>',
+      closingTag: '</h1>',
+      aliases: ['heading1', 'heading 1', 'headingone', 'heading one', 'h1']
+    }
+  ];
 
   function reset() {
     recognizing = false;
@@ -73,18 +99,67 @@ $(document).ready(function(){
     toggleStartStop();
   });
 
+
+  // SUBMIT BUTTON CLICK FUNCTION
   submitToChat.on('click', function() {
     event.preventDefault();
     submittedChat = chatTextArea.val().trim();
     chatTextArea.empty();
     chatTextArea.val('');
+
     console.log(submittedChat);
     splitIntoArray(submittedChat);
     console.log(sentenceArray);
 
-    checkTextForElement();
+    let result = checkSubmittedTextForElement();
+    // console.log("final result:", result);
+    elementMatchCount = 0; // resets element match count for future checks
+
+    if (result === "No matching elements found") {
+      console.log("I did not find any matching elements in your statement");
+    }
+    else if (result === "More than one element match found") {
+      console.log("I can only create one element at a time.  Which would element do you want to create first?");
+    }
+    else {
+      let selectedElement = elementsObjectsArray[result].name;
+      let selectedElementOpenTag = elementsObjectsArray[result].openTag;
+      let selectedElementClosingTag = elementsObjectsArray[result].closingTag;
+      console.log(selectedElement, selectedElementOpenTag, selectedElementClosingTag);
+      // checkTextForElement()
+    }
+
     recognition = "";
   });
+
+  function checkSubmittedTextForElement() {
+    sentenceArray.forEach(function(word) {
+      elementsObjectsArray.forEach(function(element) {
+        let aliasesArray = element.aliases;
+        aliasesArray.forEach(function(alias) {
+          if (alias === word) {
+            elementMatchCount++;
+            elementObjectIndex = element.position;
+            // console.log("match found: " + alias + " in: " + element.name + " index: " + elementObjectIndex)
+          }
+        });
+      });
+    });
+    switch (elementMatchCount) {
+      // NO MATCHES FOUND
+      case 0:
+        return "No matching elements found";
+
+      // ONY ONE MATCH FOUND
+      case 1:
+        // console.log("element referenced:", elementsObjectsArray[elementObjectIndex].name);
+        return elementObjectIndex;
+
+      // MORE THAN ONE ELEMENT MATCH IS FOUND
+      default:
+        return "More than one element match found";
+    }
+  }
 
   function tagConstructor (elementTag, elementID, elementClass) {
     // NEEDS DEFINITITION
