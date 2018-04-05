2bbd22b8474f33aac4cf6efa783c98211b733837
Geoff's Macbook <geoffsmacbook@lawn-143-215-114-18.lawn.gatech.edu>
2018-04-05 16:18:07 -0400

    saving progress and pulling in updated Master branch

---
+++ assets/javascript/elementsLibrary.js
@@ -0,0 +1,114 @@
+const elementsObjectsArray = [
+  {
+    position: 0,
+    name: 'div',
+    openTag: '<div',
+    closingTag: '</div>',
+    aliases: ['division', 'divider', 'div']
+  },
+  {
+    position: 1,
+    name: 'p',
+    openTag: '<p',
+    closingTag: '</p>',
+    aliases: ['paragraph', 'par', 'p']
+  },
+  {
+    position: 2,
+    name: 'h1',
+    openTag: '<h1',
+    closingTag: '</h1>',
+    aliases: ['heading1', 'heading 1', 'headingone', 'heading one', 'h1', 'H1', 'header1', 'header 1', 'headerone', 'header one']
+  },
+  {
+    position: 3,
+    name: 'h2',
+    openTag: '<h2',
+    closingTag: '</h2>',
+    aliases: ['heading12', 'heading 2', 'headingtwo', 'heading two', 'h2', 'H2', 'header2', 'header 2', 'headertwo', 'header two']
+  },
+  {
+    position: 4,
+    name: 'h3',
+    openTag: '<h3',
+    closingTag: '</h3>',
+    aliases: ['heading3', 'heading 3', 'headingthree', 'heading three', 'h3', 'H3', 'header3', 'header 3', 'headerthree', 'header three']
+  },
+  {
+    position: 5,
+    name: 'h4',
+    openTag: '<h4',
+    closingTag: '</h4>',
+    aliases: ['heading4', 'heading 4', 'headingfour', 'heading four', 'h4', 'H4', 'header4', 'header 4', 'headerfour', 'header four']
+  },
+  {
+    position: 6,
+    name: 'h5',
+    openTag: '<h5',
+    closingTag: '</h5>',
+    aliases: ['heading5', 'heading 5', 'headingfive', 'heading five', 'h5', 'H5', 'header5', 'header 5', 'headerfive', 'header five']
+  },
+  {
+    position: 7,
+    name: 'h6',
+    openTag: '<h6',
+    closingTag: '</h6>',
+    aliases: ['heading6', 'heading 6', 'headingsix', 'heading six', 'h6', 'H6', 'header6', 'header 6', 'headersix', 'header six']
+  },
+  {
+    position: 8,
+    name: 'a',
+    openTag: '<a',
+    closingTag: '</a>',
+    aliases: ['anchor', 'link']
+  },
+  {
+    position: 9,
+    name: 'table',
+    openTag: '<table',
+    closingTag: '</table>',
+    aliases: ['table']
+  },
+  {
+    position: 10,
+    name: 'tr',
+    openTag: '<tr',
+    closingTag: '</tr>',
+    aliases: ['table row', 'tablerow']
+  },
+  {
+    position: 11,
+    name: 'th',
+    openTag: '<th',
+    closingTag: '</th>',
+    aliases: ['table header', 'tableheader', 'table heading', 'tableheading']
+  },
+  {
+    position: 12,
+    name: 'td',
+    openTag: '<td',
+    closingTag: '</td>',
+    aliases: ['table data', 'tabledata']
+  },
+  {
+    position: 13,
+    name: 'ul',
+    openTag: '<ul',
+    closingTag: '</ul>',
+    aliases: ['unordered list', 'unorderedlist']
+  },
+  {
+    position: 14,
+    name: 'li',
+    openTag: '<li',
+    closingTag: '</li>',
+    aliases: ['listitem', 'list item']
+  },
+  {
+    position: 15,
+    name: 'img',
+    openTag: '<img',
+    closingTag: '</img>',
+    aliases: ['image', 'picture']
+  }
+];
