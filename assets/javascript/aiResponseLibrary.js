f6fca659b9ec2e47b4ceba4a54ad1400e5475efc
Geoff's Macbook <geoffsmacbook@Geoffss-MacBook-Pro.local>
2018-04-06 21:31:05 -0400

    updated response when no elements found

--- assets/javascript/aiResponseLibrary.js
+++ assets/javascript/aiResponseLibrary.js
@@ -1,7 +1,7 @@
 const computerMessages = {
   welcomeMessage: 'Welcome to The Code Dictator!  I\'m your benevolent dictator and I\'m here to help.  I\'ve created a standard HTML tree for you.  Tell me what you\'d like to do next.',
   spellingCorrected: 'I believe you meant... "',
-  noElementsFound: 'I could not find any matching elements in your statement',
+  noElementsFound: 'I\'m afraid I don\'t understand what you mean',
   tooManyElementsFound: 'I can only create one element at a time.  Which element do you want to create first?',
   creatingElement: 'If I understand correctly, you are looking for an element tag creation.  Coming right up!',
   meaningOfLife: 'It\'s 42, obviously!  Or at least that\'s what Doug would have us believe...',
